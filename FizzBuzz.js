24ee22fd94db7ca9dd633638cb219dc6e17fdeb3
DESKTOP-6LCURLL\Yanagita <yosiphi@hotmail.com>
2022-01-22 22:17:43 +0900

    2022/1/22 JavaScript課題2修正 html構造等正しく＋動的に

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,37 +1,46 @@
 const FizzBuzz = (fizzNum, buzzNum) => {
     let outputtext = '';
-    const htmlout = document.getElementById("htmlout");
+    const output = document.getElementById("output");
+    output.textContent = null;
     outputtext = checkfizzbuzzInput(fizzNum);
     if(outputtext !== ''){
-        htmlout.innerHTML = `fizzNumには${outputtext}`;
+        const p = document.createElement('p');
+        p.textContent = `fizzNumには${outputtext}`
+        output.appendChild(p);
         return;
     }
     outputtext = checkfizzbuzzInput(buzzNum);
     if(outputtext !== ''){
-        htmlout.innerHTML = `buzzNumには${outputtext}`;
+        const p = document.createElement('p');
+        p.textContent = `buzzNumには${outputtext}`
+        output.appendChild(p);
         return;
     }
-    htmlout.innerHTML = "表示したい文字列";
-        for( let i = Math.min(fizzNum,buzzNum) ; i<=99 ; i++ ){
-            if(i % fizzNum === 0 && i% buzzNum === 0){
-                outputtext += `FizzBuzz ${i}<br>`;
-            }
-            else if(i % fizzNum === 0){
-                outputtext += `Fizz ${i}<br>`;
-            }
-            else if(i % buzzNum === 0){
-                outputtext += `Buzz ${i}<br>`;
-            }
+    const ul = document.createElement('ul');
+    output.appendChild(ul);
+    for( let i = 1 ; i<=99 ; i++ ) {
+        const li = document.createElement('li');
+        if(i % fizzNum === 0 && i % buzzNum === 0) {
+            li.textContent = `FizzBuzz ${i}`;
+            ul.appendChild(li);
         }
-    htmlout.innerHTML = outputtext;
+        else if(i % fizzNum === 0){
+            li.textContent = `Fizz ${i}`;
+            ul.appendChild(li);
+        }
+        else if(i % buzzNum === 0){
+            li.textContent = `Buzz ${i}`;
+            ul.appendChild(li);
+        }
+    }
 }
 
 let checkfizzbuzzInput = (value) => {
-//入力値のチェック
+    value = value.trim();
     if(value === ''){
         return '空白が入力されています。';
     }
-    if(Number.isInteger(Number(value)) === false){
+    if(Number.isInteger(Number(value)) === false) {
         return '整数値を入力してください。';
     }
     return '';
